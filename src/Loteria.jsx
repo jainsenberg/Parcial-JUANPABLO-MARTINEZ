90a3313ab3b61fd569f12eafa7b56733ad2c7841
JUAN PABLO MARTINEZ GUZMAN <juan_p.martinez_g@uao.edu.co>
2023-03-07 21:22:34 -0500

    NUEVO PARCIAL

--- src/Loteria.jsx
+++ src/Loteria.jsx
@@ -1,30 +1,62 @@
 import React from 'react'
 import { useState } from 'react'
 
-const loterias = ['VALLE', 'ANTIOQUIA', 'GUAJIRA']
-var numeroE =1;
-var precio = 1000
+const loteriass = [
+    {id: 1, lote: 'CALI'},
+    {id: 2, lote: 'GUAJIRA'},
+    {id: 3, lote: 'PETECUY'}
+]
+const [loteriaV, setLoteria] =useState('')
+const [numeroV, setNum] =useState('')
+const [precioV, setPri] =useState('')
 
-console.log()
 
+const entradaLoteria = (event)=>{
+    setLoteria(event.target.value)
 
+}
+const entradaNum= (event) =>{
+    setNum(event.target.value)
+}
+const entradaPrice= (event) =>{
+    setPri(event.target.value)
+}
+
+const enviar =(event) =>{
+    event.preventDefault(),
+    setLoteria(''),
+    setNum(''),
+    setPri('')
+}
 
 const Loteria = () => {
     return (
         
-    <div>Loterias
+    <div>
+        <h1>CHOCLITOS MILLONARIO</h1>
+        <h2>Gane picantico cobre sabrosito</h2>
         
-        <select name="LOTERIAS" id="1">
-            <option value="1">VALLE</option>
-            <option value="2">CAUCA</option>
-            <option value="3">GUADALAJARA</option>
+        <select value={loteriaV} onChange={(event) => entradaLoteria(event)}>
+            {loteriass.map((item, i)=>(
+                <option key={"Id"+i} value={item.lote}>{item.id}) {item.lote}</option>
+            ))}
 
         </select>
-        <input  minLength="3" maxLength="4" placeholder="NUMERO QUE QUIERA"></input>
-        <input  placeholder="PRECIO"></input>
-        <button></button>
+        <input  minLength="3" maxLength="4" placeholder="NUMERO QUE QUIERA" onChange={(event) => entradaNum(event)}></input>
+        <input  placeholder="PRECIO" onChange={(event) => entradaPrice(event)}></input>
+        <button guardar></button>
+
+        <table>
+            <thead>
+                <tr>
+                    <th>LOTERIA</th><th>NUMERO</th><th>PRECIO</th><th>TOTAL</th>
+                </tr>
+            </thead>
+            <tr>
+                <th>###</th> 
+            </tr>
+        </table>
     </div>
-    
   )
 }
 
